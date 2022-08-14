c6f957bff4bfa693b47e9c8e88d2f8a1f7f64b07
EmilMurahas228 <murakhas@list.ru>
2022-08-14 22:04:53 +0300

    feat: add extensions in files

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 
 app.use(cors());
 app.use(express.static("public"));
+app.use(express.static("default/img"));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -22,9 +23,45 @@ app.post("/upload", (req, res) => {
   upload(req, res, (err) => {
     if (err) {
       return res.status(500).json(err);
+    } else {
+      let newFiles = {};
+
+      const extensions = [
+        {
+          value: [".txt", ".docx", ".pdf", ".accdb", ".doc", ".ppt"],
+          src: "img/text.png",
+        },
+        {
+          value: [".rar", ".zip"],
+          src: "img/winrar.png",
+        },
+        {
+          value: [".mp4"],
+          src: "img/video.png",
+        },
+        {
+          value: [".gif", ".png", ".webp"],
+        },
+      ];
+
+      const files = req.files.map((item) => {
+        newFiles = item;
+        newFiles["ext"] = path.extname(item.originalname);
+
+        extensions.map((item) => {
+          if (item.value.includes(newFiles.ext)) {
+            newFiles.src = item.src;
+            if (!item.src) {
+              newFiles.src = newFiles.filename;
+            }
+          }
+        });
+
+        return newFiles;
+      });
+
+      res.status(200).send(files);
     }
-    console.log(req.files);
-    return res.status(200).send(req.files);
   });
 });
 
