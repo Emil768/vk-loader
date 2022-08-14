5c5d42b1297cb843fecad1cfdf8e3951244ecc5b
EmilMurahas228 <murakhas@list.ru>
2022-08-14 15:19:29 +0300

    fead: add search

--- vk-loader/src/components/LoaderPanel/LoaderPanel.jsx
+++ vk-loader/src/components/LoaderPanel/LoaderPanel.jsx
@@ -8,6 +8,7 @@ import "../MyButton/MyButton.scss";
 function LoaderPanel() {
   const [modal, setModal] = useState(false);
   const [files, setFiles] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   const openModal = () => {
     setModal(true);
@@ -17,6 +18,11 @@ function LoaderPanel() {
     setFiles(newfiles);
     setModal(false);
   };
+
+  const filterFiles = files.filter(
+    (file) =>
+      file.originalname.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1
+  );
 
   return (
     <div className="loader">
@@ -28,12 +34,30 @@ function LoaderPanel() {
           Загрузить файл
         </div>
       </div>
-      <input type="text" className="loader__input" placeholder="Поиск файлов" />
+      <input
+        type="text"
+        className="loader__input"
+        placeholder="Поиск файлов"
+        onChange={(e) => setSearchValue(e.target.value)}
+        maxLength={50}
+      />
       <div className="loader__content">
-        <div className="loader__list">
-          {files.map((file, index) => (
-            <LoaderItem key={index} file={file} />
-          ))}
+        <div
+          className={
+            filterFiles.length
+              ? "loader__list "
+              : "loader__list loader__list-empty"
+          }
+        >
+          {filterFiles.length ? (
+            filterFiles.map((file, index) => (
+              <LoaderItem key={index} file={file} />
+            ))
+          ) : (
+            <span className="loader__empty">
+              В ваших файлах по запросу «{searchValue}» ничего не найдено
+            </span>
+          )}
         </div>
       </div>
       <MyModal visible={modal} setVisible={setModal} onSucces={onSucces} />
